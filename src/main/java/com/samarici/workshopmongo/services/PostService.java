3100e6fce54df95f54612c4437e44cebd17bb59f
mksampei <marcelo.sampei@gmail.com>
2021-03-19 18:59:11 -0300

    Obtendo um post por id

---
+++ src/main/java/com/samarici/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.samarici.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.samarici.workshopmongo.domain.Post;
+import com.samarici.workshopmongo.repository.PostRepository;
+import com.samarici.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
